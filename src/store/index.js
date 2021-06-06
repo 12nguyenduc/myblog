fcb6d9a811cddc8d27a98f674b5237d99a5560ad
Nguyễn Đức <devnv@tima.vn>
2021-06-06 15:31:03 +0700

    update

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,6 @@ import Vuex from 'vuex'
 import modules from './modules'
 
 Vue.use(Vuex)
-Vue.use(require('vue-moment'))
 export default new Vuex.Store({
   modules,
   strict: process.env.NODE_ENV === 'development'
