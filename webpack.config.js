7f6cd066604509cb178720c4fa563d9fdc28d0c6
Nguyễn Đức <devnv@tima.vn>
2021-06-07 22:17:20 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,30 @@
 // Resolve Webpack alias in Webstorm (https://gist.github.com/zmts/bd620cd473e6c96e8884f03d0cee7f15)
-module.exports = {
-  module: {
-    loaders: [
-      { include: /\.json$/, loaders: ['json-loader'] }
-    ]
-  },
-  resolve: {
-    alias: {
-      '@': require('path').resolve(__dirname, 'src'),
-      extensions: ['', '.json', '.jsx', '.js']
-    }
+// module.exports = {
+//   module: {
+//     loaders: [
+//       { include: /\.json$/, loaders: ['json-loader'] }
+//     ]
+//   },
+//   resolve: {
+//     alias: {
+//       '@': require('path').resolve(__dirname, 'src'),
+//       extensions: ['', '.json', '.jsx', '.js']
+//     }
+//   }
+// }
+
+var config = {
+  entry: './app.js'
+}
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map'
   }
+
+  if (argv.mode === 'production') {
+    // ...
+  }
+
+  return config
 }
